6510e46e4476ffedb483c237ddf0c494c59f171a
antonfirsov <antonfir@gmail.com>
2024-02-08 20:30:09 +0100

    .

--- test-other/MyBenchmarks/Program.cs
+++ test-other/MyBenchmarks/Program.cs
@@ -1,15 +1,16 @@
 ﻿// Copyright 2024 Michael Conrad.
 // Licensed under the Apache License, Version 2.0.
 // See LICENSE file for details.
+using BenchmarkDotNet.Running;
 using MyBenchmarks;
 
-SocketsHttpHandler handler = new SocketsHttpHandler()
-{
-    ConnectCallback = HttpBenchmarks.WftConnectHandler
-};
+//SocketsHttpHandler handler = new SocketsHttpHandler()
+//{
+//    ConnectCallback = HttpBenchmarks.WftConnectHandler
+//};
 
-HttpClient client = new HttpClient(handler);
-HttpResponseMessage response = await client.GetAsync("https://httpbin.org/bytes/100").ConfigureAwait(false);
-Console.WriteLine(response.StatusCode);
+//HttpClient client = new HttpClient(handler);
+//HttpResponseMessage response = await client.GetAsync("https://httpbin.org/bytes/100").ConfigureAwait(false);
+//Console.WriteLine(response.StatusCode);
 
-//BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
