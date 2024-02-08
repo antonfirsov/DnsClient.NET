ff90f348d500ab67ff2a2258c92c7d6abeefd9f8
antonfirsov <antonfir@gmail.com>
2024-02-08 16:17:58 +0100

    add MyBenchmarks

---
+++ test-other/MyBenchmarks/LookupBenchmarks.cs
@@ -0,0 +1,22 @@
+﻿// Copyright 2024 Michael Conrad.
+// Licensed under the Apache License, Version 2.0.
+// See LICENSE file for details.
+
+using System.Net;
+using BenchmarkDotNet.Attributes;
+
+namespace MyBenchmarks;
+
+[MemoryDiagnoser]
+public class LookupBenchmarks
+{
+    [Params("localhost")]
+    public string HostName { get; set; }
+
+    [Benchmark]
+    public void GetHostEntry() => _ = Dns.GetHostEntry(HostName);
+
+    [Benchmark]
+    public Task GetHostEntryAsync() => Dns.GetHostEntryAsync(HostName);
+}
+
---
+++ test-other/MyBenchmarks/Program.cs
@@ -0,0 +1,7 @@
+﻿// Copyright 2024 Michael Conrad.
+// Licensed under the Apache License, Version 2.0.
+// See LICENSE file for details.
+
+using BenchmarkDotNet.Running;
+
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
